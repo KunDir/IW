26fe65f425241335aedfdba821a09739315d1e37
FKLam <lfk0120@163.com>
2015-01-22 17:06:32 +0800

    After merge devV1.0

--- ItcastWeibo/ItcastWeibo/Classes/Home(首页)/Controller/IWHomeViewController.h
+++ ItcastWeibo/ItcastWeibo/Classes/Home(首页)/Controller/IWHomeViewController.h
@@ -4,11 +4,7 @@
 //
 //  Created by kun on 15/1/16.
 //  Copyright (c) 2015年 kun. All rights reserved.
-<<<<<<< HEAD
-//  首页 （master）
-=======
-//  首页 
->>>>>>> devV1.0
+//  首页
 
 #import <UIKit/UIKit.h>
 
