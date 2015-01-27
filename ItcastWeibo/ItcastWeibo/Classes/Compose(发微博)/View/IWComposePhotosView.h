07acc5f70a471f8003c9e84a24dda1d53992bfc7
FKLam <lfk0120@163.com>
2015-01-27 09:17:01 +0800

    发微博－发多张图片的微博

---
+++ ItcastWeibo/ItcastWeibo/Classes/Compose(发微博)/View/IWComposePhotosView.h
@@ -0,0 +1,14 @@
+//
+//  IWComposePhotosView.h
+//  ItcastWeibo
+//
+//  Created by kun on 15/1/27.
+//  Copyright (c) 2015年 kun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IWComposePhotosView : UIView
+- (void)addImage:(UIImage *)image;
+- (NSArray *)images;
+@end
